35087a648b1826fdf0d5f55cdbafcce467552355
Tomas Morales <tmorales1@uc.cl>
2017-09-23 20:32:13 -0300

    print comentados

--- crear_guia.py
+++ crear_guia.py
@@ -14,25 +14,27 @@ def crear_guia(input_doc_name, output_doc_name, sep, questions):
 
 	#encontar separadores y meter sus indices en seps
 	seps = [0]
-	count = 0
+	# count = 0
 	for i in range(len(paragraphs)):
 		if paragraphs[i].text == sep:
-			count += 1
+			# count += 1
 			seps.append(i)
 			paragraphs[i].text = ''
-	print(seps)
-	print(count)
+	# print(seps)
+	# print(count)
 
 	#eliminar preguntar no requeridas
 	inicio = 0
 	fin = 0
 	for q in questions:
 		fin = seps[q-1]
-		print(inicio, fin)
+		# print(inicio, fin)
+		#inicio dejar preguntas juntas!
 		if inicio == fin:
 			for x in range(fin + 1, seps[q] - 1):
 				if paragraphs[x].text != '':
 					paragraphs[x].paragraph_format.keep_with_next = True
+		#fin dejar preguntas juntas
 		for j in range(inicio, fin):
 			delete_paragraph(paragraphs[j])
 		inicio = seps[q]
