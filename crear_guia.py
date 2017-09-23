af1bf7d7d318c7e4276faaa0e4748a943a567111
Tomas Morales <tmorales1@uc.cl>
2017-09-23 20:30:49 -0300

    preguntas juntas siemrpe

--- crear_guia.py
+++ crear_guia.py
@@ -14,11 +14,14 @@ def crear_guia(input_doc_name, output_doc_name, sep, questions):
 
 	#encontar separadores y meter sus indices en seps
 	seps = [0]
+	count = 0
 	for i in range(len(paragraphs)):
 		if paragraphs[i].text == sep:
+			count += 1
 			seps.append(i)
 			paragraphs[i].text = ''
 	print(seps)
+	print(count)
 
 	#eliminar preguntar no requeridas
 	inicio = 0
@@ -26,14 +29,19 @@ def crear_guia(input_doc_name, output_doc_name, sep, questions):
 	for q in questions:
 		fin = seps[q-1]
 		print(inicio, fin)
+		if inicio == fin:
+			for x in range(fin + 1, seps[q] - 1):
+				if paragraphs[x].text != '':
+					paragraphs[x].paragraph_format.keep_with_next = True
 		for j in range(inicio, fin):
 			delete_paragraph(paragraphs[j])
 		inicio = seps[q]
 	for i in range(inicio, len(paragraphs)):
 		delete_paragraph(paragraphs[i])
 
+
 	#crear nueva guia
 	document.save(output_doc_name)
 
 
-crear_guia('invierno.docx', 'output1.docx', '*', [1, 5, 7, 15, 18])
+crear_guia('invierno.docx', 'output1.docx', '*', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 18, 20])
